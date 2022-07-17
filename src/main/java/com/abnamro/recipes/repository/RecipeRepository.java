dd675878d96ed379d19afe54820ae7b1a1ea5e62
omer-yusufoglu <omer.yusufoglu@vodafone.com>
2022-07-17 12:08:27 +0300

    add javadoc comment

--- src/main/java/com/abnamro/recipes/repository/RecipeRepository.java
+++ src/main/java/com/abnamro/recipes/repository/RecipeRepository.java
@@ -3,5 +3,8 @@ package com.abnamro.recipes.repository;
 import com.abnamro.recipes.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repository class for crud operations
+ */
 public interface RecipeRepository extends CrudRepository<Recipe, String>, CustomRecipeRepository{
 }
