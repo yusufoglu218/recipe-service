a562b1a0d9ef51e68acdbd7d511e3863596eecfb
omer-yusufoglu <omer.yusufoglu@vodafone.com>
2022-07-19 01:03:42 +0300

    add recipe id for delete case

--- src/test/java/com/abnamro/recipes/constant/TestConstants.java
+++ src/test/java/com/abnamro/recipes/constant/TestConstants.java
@@ -5,7 +5,9 @@ public class TestConstants {
     private TestConstants() {
     }
 
-    public static final String RECIPE_ID = "62d3ca373e968f1d5b4afd30";
+    public static final String RECIPE_ID = "Id123456";
+    public static final String RECIPE_ID_TO_DELETE = "IdDelete123456";
+
     public static final String INSTRUCTIONS = "test instructions";
     public static final Integer NUMBER_OF_SERVING = 5;
 
