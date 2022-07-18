60f4ec6a6a5a0ab7dc5f95411a3aba9da7b7b46e
omer-yusufoglu <omer.yusufoglu@vodafone.com>
2022-07-18 14:36:50 +0300

    constant class for test objects

---
+++ src/test/java/com/abnamro/recipes/constant/TestConstants.java
@@ -0,0 +1,18 @@
+package com.abnamro.recipes.constant;
+
+public class TestConstants {
+
+    private TestConstants() {
+    }
+
+    public static final String RECIPE_ID = "62d3ca373e968f1d5b4afd30";
+    public static final String INSTRUCTIONS = "test instructions";
+    public static final Integer NUMBER_OF_SERVING = 5;
+
+    public static final String INSTRUCTIONS_TO_UPDATE = "test instructions to update";
+    public static final Integer NUMBER_OF_SERVING_TO_UPDATE = 3;
+
+    public static final String INGREDIENT_NAME = "test ingredient";
+    public static final Boolean INGREDIENT_IS_VEGETARIAN_TRUE = true;
+    public static final Boolean INGREDIENT_IS_VEGETARIAN_FALSE = false;
+}
